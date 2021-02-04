ac5296dd57d0c9f50301945c21e745d629aff3d4
Joshua Smerdel <josh@smerdel.net>
2021-02-04 11:21:51 -0500

    JOSH - Accounts server save

--- module-2/15_Capstone/pair-exercise/java/tenmo-server/src/main/java/com/techelevator/tenmo/model/Accounts.java
+++ module-2/15_Capstone/pair-exercise/java/tenmo-server/src/main/java/com/techelevator/tenmo/model/Accounts.java
@@ -1,5 +1,31 @@
 package com.techelevator.tenmo.model;
 
+import java.math.BigDecimal;
+
 public class Accounts {
+	
+	private int accountID;
+	private int userID;
+	private BigDecimal balance;
+	
+	
+	public int getAccountID() {
+		return accountID;
+	}
+	public void setAccountID(int accountID) {
+		this.accountID = accountID;
+	}
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public BigDecimal getBalanceBigDecimal() {
+		return balance;
+	}
+	public void setBalanceBigDecimal(BigDecimal balance) {
+		this.balance = balance;
+	}
 
 }
