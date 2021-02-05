4e3fe77ee04f4413f7cacc55703b351e6dc75154
Joshua Smerdel <josh@smerdel.net>
2021-02-05 16:41:56 -0500

    JOSH - Update to queries.

--- module-2/15_Capstone/pair-exercise/SQL queries.sql
+++ module-2/15_Capstone/pair-exercise/SQL queries.sql
@@ -1,2 +1,18 @@
+-- TRANSFER TO
+INSERT INTO transfers (transfer_type_id, transfer_status_id, account_from, account_to, amount)
+VALUES (2, 2, ?, ?, ?);
+
+-- UPDATING BALANCE TABLE
+UPDATE accounts
+SET balance = balance - ?
+WHERE account_id = ?;
+
+UPDATE accounts
+SET balance = balance + ?
+WHERE account_id = ?;
+
+SELECT *
+FROM transfers;
+
 select *
 from accounts;
